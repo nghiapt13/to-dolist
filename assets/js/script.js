6775890503dd10a8e16ef7985a74c753a895cf64
nghiapt13 <fb.trungnghia13@gmail.com>
2024-02-21 13:25:17 +0700

    add block copy function

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,92 @@ function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showList(){
-    listContainer.innerHTML=localStorage.getItem("data");
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 
 showList();
+
+//Chống copy
+function killCopy(e) {
+    return false;
+}
+
+function reEnable() {
+    return true;
+}
+
+document.onselectstart = new Function("return false");
+
+if (window.sidebar) {
+    document.onmousedown = killCopy;
+    document.onclick = reEnable;
+}
+
+function noteOut() {
+    var note = document.querySelector(".note");
+    note.style.display = "none";
+}
+
+setInterval(noteOut, 3000);
+
+//Chống chuột phải 
+window.onload = function () {
+    var copyText = document.getElementById('copy-text').innerHTML;
+    navigator.clipboard.writeText(copyText);
+    document.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+        alert("Ctrl V để thấy bất ngờ");
+        copyText();
+    }, false);
+    document.addEventListener("keydown", function (e) {
+        //document.onkeydown = function(e) {
+        // "I" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+            disabledEvent(e);
+        }
+        // "J" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            disabledEvent(e);
+        }
+        // "S" key + macOS
+        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+            disabledEvent(e);
+        }
+        // "U" key
+        if (e.ctrlKey && e.keyCode == 85) {
+            disabledEvent(e);
+        }
+        // "F12" key
+        if (event.keyCode == 123) {
+            disabledEvent(e);
+        }
+    }, false);
+
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else if (window.event) {
+            window.event.cancelBubble = true;
+        }
+        e.preventDefault();
+        return false;
+    }
+};
+
+//Chống Ctrl + U
+document.onkeydown = function (e) {
+    if (e.ctrlKey &&
+        (e.keyCode === 67 ||
+            e.keyCode === 86 ||
+            e.keyCode === 85 ||
+            e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+$(document).keypress("u", function (e) {
+    if (e.ctrlKey) return false;
+    else return true;
+});
